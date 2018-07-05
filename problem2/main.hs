f9fb9e378eb8dc8fab0a62f50994abd4b15cc359
KeisukeToyota <hm.pudding0715@gmai.com>
2018-07-05 19:58:12 +0900

    commit

--- problem2/main.hs
+++ problem2/main.hs
@@ -1,12 +1,19 @@
-fib n
-  | n < 2 = n
-  | otherwise = fib(n-1) + fib(n-2)
+--fib 0 = 0
+--fib 1 = 1
+--fib n = (memo !! (n-1) + memo !! (n-2))
+--memo = [fib x  | x <- [0,1..]]
+fib = (map memo [0 ..] !!)
+  where
+    memo 0 = 0
+    memo 1 = 1
+    memo n = fib(n-1) + fib(n-2)
 
-takeFib n p =
 
-evenFib n
-  | fib(n)%2
+fibList n = [x | x <- map fib [1..100], x <= n]
+evenList list = filter (\x -> x `mod` 2 == 0) list
+evenSumFib n = sum $ evenList $ fibList $ n
 
 main::IO()
 main = do
-  print $ fib(20)
+  print $ evenSumFib $ 4000000
+  print $ fib 10000
